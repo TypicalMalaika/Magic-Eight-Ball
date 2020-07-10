521d417516505b71e16d0b5f498c94c4239420a0
Malaika Karim <42036621+TypicalMalaika@users.noreply.github.com>
2020-07-10 16:21:09 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+var userName = "Jane";
+userName = "Jane" ? console.log(`Hello ${userName}!`) : console.log("Hello");
+var userQuestion = "Will it rain today?";
+console.log(userQuestion);
+var randomNumber = Math.floor(Math.random()*8); //this number is assigned to the ball outcome
+var eightBall = "" //this is the ouput statememt
+
+switch (randomNumber){
+  case randomNumber = 0:
+    eightBall = "It is certain";
+    break 
+  case randomNumber = 1:
+    eightBall = "It is decidedly so";
+    break 
+  case randomNumber = 2:
+    eightBall = "Reply hazy try again";
+    break
+  case randomNumber = 3:
+    eightBall = "Cannot predit now";
+    break
+  case randomNumber = 4:
+    eightBall = "Do not count on it";
+    break
+  case randomNumber = 5:
+    eightBall = "My sources say no";
+    break
+  case randomNumber = 6:
+    eightBall = "Outlook not so good";
+    break
+  case randomNumber = 7:
+    eightBall = "Signs point to yes";
+    break 
+}
+
+for (i <= 7){
+  i++
+  console.log(i)
+}
+  /*
+if (randomNumber=0){
+  console.log("0")
+} else if (randomNumber=1){
+  console.log("1")
+} else if (randomNumber=2){
+  console.log("2")
+} else if (randomNumber=3){
+  console.log("3")
+} else if (randomNumber=4){
+  console.log("4")
+} else if(ran)
+
+console.log(eightBall)
